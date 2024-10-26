9c1ad2d2d0290a0927fafef4cb7266b47158da2d
Jakub Grad <jakub.grad@icloud.com>
2024-10-26 23:14:44 +0300

    Added poetry and 100% coverage for varasto

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -37,4 +37,44 @@ class TestVarasto(unittest.TestCase):
         self.varasto.ota_varastosta(2)
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
-        self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+        self.assertAlmostEqual(self.varasto.tilavuus, 10)
+
+    def test_volume_cannot_be_negative(self):
+        varasto = Varasto(-10)
+
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+    
+    def test_initial_saldo_cannot_be_negative(self):
+        varasto = Varasto(10,-10)
+
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_cannot_add_negative_amount(self):
+        self.varasto = Varasto(10)
+
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_cannot_add_more_than_maximum_volume(self):
+        self.varasto = Varasto(10)
+
+        self.varasto.lisaa_varastoon(100)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+    
+    def test_cannot_take_out_negative_amount_from_storage(self):
+        self.varasto = Varasto(10,10)
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-1), 0)
+
+    def test_cannot_take_out_more_than_is_in_the_storage(self):
+        self.varasto = Varasto(10,10)
+
+        self.assertAlmostEqual(self.varasto.ota_varastosta(100), 10)
+
+    def test_str_function_works(self):
+        self.varasto = Varasto(10,10)
+
+        self.assertEqual(str(self.varasto), "saldo = 10, vielä tilaa 0")
+    
